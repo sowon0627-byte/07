07da971f67c34323250241fb04e272df62dbbf1f
sowon0627-byte <sowon0627@sookmyung.ac.kr>
2025-10-16 15:26:07 +0900

    실습 2_2

--- main.c
+++ main.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int inc(int counter);
+
 int main(void)
 {
     int i;
     
     for(i=0;i<5;i++)
     {
-                    int temp = 1;
+                    static int temp = 1;
                     printf("temp = %d\n", temp);
                     temp++;
-    }        
-  
+    }
+    
   system("PAUSE");	
   return 0;
 }
